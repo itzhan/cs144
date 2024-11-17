6eac79ea3e4baf5ca28befb8f88a419030b40096
cs144 <2802965114@qq.com>
2024-11-17 06:53:24 +0000

    lab04--done

--- apps/webget.cc
+++ apps/webget.cc
@@ -4,13 +4,14 @@
 #include <iostream>
 #include <span>
 #include <string>
+#include "tcp_minnow_socket.hh"
 
 using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
   const Address addressServer( host, "http" );
-  TCPSocket socket;
+  CS144TCPSocket socket;
   socket.connect( addressServer );
 
   std::string request = "GET " + path + " HTTP/1.1\r\n";
